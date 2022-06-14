a96158e33e04e93f7225a98164a4035414d6743e
Sagnik Das <sagnikdas53@live.com>
2022-06-14 18:38:00 +0530

    make the JS needed to send the bulk requests

--- static/script.js
+++ static/script.js
@@ -1,5 +1,7 @@
 const url = "http://127.0.0.1:5000/";
 var tokenID = ""
+var picker = ''
+
 function keyEvent(event) {
     event.preventDefault();
     if (event.key == "Enter") {
@@ -76,13 +78,14 @@ async function regcmp() {
     }
 }
 
+async function noReg() {
+    const element = document.getElementById("rForm");
+    element.remove();
+    addbotMsgRTL('Cancelled Registration');
+    login(true);
+}
+
 function login(isChained = false) {
-    // make it so that the next message can be used to register directly
-    //console.log(isChained);
-    if (isChained) {
-        const element = document.getElementById("rForm");
-        element.remove();
-    }
     var formDiv = document.createElement("div");
     var myRequest = new Request(url + "lform.html");
     fetch(myRequest).then(async function (response) {
@@ -99,8 +102,8 @@ function login(isChained = false) {
 }
 
 async function loginHandel() {
-    var email = document.getElementById("InputEmail1").value;
-    var pword = document.getElementById("InputPassword1").value;
+    var email = document.getElementById("InputEmail2").value;
+    var pword = document.getElementById("InputPassword2").value;
     //console.log(email, pword);
 
     //addbotMsgRTL("Logging you in");
@@ -140,6 +143,74 @@ async function loginHandel() {
     }
 }
 
+async function loginCancel() {
+    const element = document.getElementById("lForm");
+    element.remove();
+    addbotMsgRTL('Cancelled Login');
+}
+
+function OOD_form() {
+    var formDiv = document.createElement("div");
+    var myRequest = new Request(url + "ODDL.html");
+    fetch(myRequest).then(async function (response) {
+        const text = await response.text();
+        formDiv.innerHTML = text;
+    });
+    var ul = document.getElementById("chat-list");
+    var li = document.createElement("li");
+    li.appendChild(formDiv);
+    li.setAttribute("class", "bot");
+    li.setAttribute("id", "OODLForm");
+    ul.appendChild(li);
+    li.scrollIntoView({ behavior: "smooth" });
+}
+
+// make this one work
+async function OOD_form_handel() {
+    var email = document.getElementById("InputEmail2").value;
+    var pword = document.getElementById("InputPassword2").value;
+
+    try {
+        // Create request to api service
+        const req = await fetch(url + "api/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+            },
+
+            // format the data
+            body: JSON.stringify({
+                email: email,
+                password: pword,
+                action: "login",
+            }),
+        });
+
+        const res = await req.json();
+
+        // Log success message
+        console.log(res);
+        if (res["status"] == "done" && res['token'] != null) {
+            addbotMsgRTL("Done");
+            const element = document.getElementById("lForm");
+            element.remove();
+            // cookies set
+            Cookies.set("token", res['token'], { expires: 7 });
+            tokenID = Cookies.get('token');
+        } else {
+            addbotMsgRTL(res["status"]);
+        }
+    } catch (err) {
+        console.error(`ERROR: ${err}`);
+    }
+}
+
+async function OOD_form_Cancel() {
+    const element = document.getElementById("OODLForm");
+    element.remove();
+    addbotMsgRTL('Removing Form');
+}
 
 async function logOutHandel() {
     if (Cookies.get('token')) {
@@ -203,6 +274,9 @@ async function adduserMsg() {
             }
             if (res["class"][0]["intent"] == "logout") {
                 logOutHandel();
+            }
+            if (res["class"][0]["intent"] == "OOD_form") {
+                OOD_form();
             }
             else {
                 addbotMsg(res["results"]);
